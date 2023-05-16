3a2a3c55955533f949810e173ee668e75deba4a5
JonathanF1210 <jonathanf1210@gmail.com>
2023-05-16 16:53:57 -0500

    Initial commit

---
+++ src/test/java/my/jonathan/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package my.jonathan.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
